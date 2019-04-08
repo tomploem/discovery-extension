d762b3d4e9c2cc259bfe4078138636901176afe2
Tom Ploem <tom.ploem@gmail.com>
2019-04-08 09:16:51 +0200

    initial try-out new layout of plugin

--- LiskDiscovery1.js
+++ LiskDiscovery1.js
@@ -1,25 +1,141 @@
-import React from 'react'
-import LiskHubExtensions from 'LiskHubExtensions'
+import React, { Component, Fragment } from 'react';
+import LiskHubExtensions from 'liskHubExtensions';
 
-class Hello extends React.Component {
+const Box = LiskHubExtensions.components.Box;
+const { moment, grid } = LiskHubExtensions.dependencies;
+const styles = LiskHubExtensions.css;
+const URI = 'https://api.liskdiscovery.com/projects';
 
-  componentDidMount() {
-    console.log(LiskHubExtensions.identifiers);
+const Project = ({ project = {} }) => {
+  const date = moment(project.date_updated);
+  const url = "https://www.liskdiscovery.com" + project.resource_url;
+  return (
+    <div className={`${grid.row} ${styles.row}`} >
+      <div className={`${grid['col-xs-6']} ${grid['col-sm-4']} ${grid['col-lg-3']}`}>
+        {project.name}
+      </div>
+        <div className={`${grid['col-sm-3']} ${grid['col-lg-3']}`}>
+        {project.type}
+      </div>
+        <div className={`${grid['col-sm-3']} ${grid['col-lg-3']} `}>
+        {date.format('DD MMM YYYY')}
+      </div>
+        <a style={{ fontSize: '12px' }} href={url} target="_blank" className={`${grid['col-xs-6']} ${grid['col-sm-3']} ${grid['col-lg-3']} `}>
+        {url}
+      </a>
+  </div>
+)
+};
+
+const Projects = ({ projects = [] }) => {
+  return (
+    <Fragment>
+    <TableHeader />
+    {
+      projects.map(project => <Project key={project.project_key} project={project} />)
+}
+</Fragment>
+)
+};
+
+const TableHeader = () => (
+  <div style={{ color: '#999' }} className={`${grid.row} ${styles.row}`}>
+<div className={`${grid['col-xs-6']} ${grid['col-sm-4']} ${grid['col-lg-3']} `}>
+Name
+</div>
+<div className={`${grid['col-sm-3']} ${grid['col-lg-3']} `}>
+Type
+</div>
+<div className={`${grid['col-sm-3']} ${grid['col-lg-3']} `}>
+Last updated
+</div>
+<div className={`${grid['col-xs-6']} ${grid['col-sm-3']} ${grid['col-lg-3']}`}>
+Url
+</div>
+</div>
+);
+
+const Placeholder = () => (
+  <div className={`${grid.row} ${styles.row}`}>
+<p>Please visit <a href="www.liskdiscovery.com" target="_blank">www.liskdiscovery.com</a> to associate projects with this account.</p>
+</div>
+);
+
+class ProjectTab extends Component{
+  constructor() {
+    super();
+
+    this.state = {
+      projects: [],
+    };
   }
 
-  render () {
+  componentDidMount() {
+    const { delegate } = this.props;
+    this.getProjects(delegate.account.address);
+  }
+
+  async getProjects(address) {
+    if (!address){
+      return;
+    }
+    try {
+      const response = await fetch(`${URI}/address/${address}`);
+      const data = await response.json();
+      if (data.projects && data.projects.length > 0){
+        this.setState({ projects: data.projects })
+      }
+    }catch(error){
+    }
+  }
+
+  renderProjects() {
+    const { projects } = this.state;
+    if (projects && projects.length > 0) {
+      return <Projects projects={projects} />
+    }else {
+      return <Placeholder />
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { delegate } = this.props;
+    if (delegate.address !== nextProps.delegate.address) {
+      this.getProjects(nextProps.delegate.account.address);
+    }
+  }
+
+  render() {
     return (
-      <LiskHubExtensions.components.Box >
-      <h1>hello world</h1>
-      </LiskHubExtensions.components.Box>
-    )
+      <Box>
+      <header>
+      <h1>Lisk Discovery projects</h1>
+    </header>
+    <main className={styles.wrapper}>
+      <div className={`${styles.results}`}>
+    {this.renderProjects()}
+  </div>
+    </main>
+    </Box>
+  );
   }
 }
 
-export default Hello
+ProjectTab.defaultProps = {
+  delegate: {
+    account: {
+      address: '',
+    }
+  },
+  tabClassName : 'delegate-statistics',
+  tabName: 'Projects'
+};
+
+export default ProjectTab;
+
 
 LiskHubExtensions.addModule({
   identifier:
-  LiskHubExtensions.identifiers.dashboardColumn1,
-  component: Hello
+  LiskHubExtensions.identifiers.delegateTab,
+  component: ProjectTab
 });
